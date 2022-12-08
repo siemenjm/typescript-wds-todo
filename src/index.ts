cb3d9da69649706e701578f69389b871f17f5340
Jared Siemen <siemenjm@gmail.com>
2022-12-07 20:29:36 -0500

    Use localstorage to persist task data

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.getElementById('new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-const tasks: Task[] = [];
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
 
 form?.addEventListener("submit", e => {
     e.preventDefault();
@@ -28,6 +29,7 @@ form?.addEventListener("submit", e => {
     };
 
     tasks.push(newTask);
+    saveTasks();
 
     addListItem(newTask);
     input.value = '';
@@ -40,7 +42,7 @@ function addListItem(task: Task) {
     
     checkbox.addEventListener('change', () => {
         task.completed = checkbox.checked;
-        console.log(tasks);
+        saveTasks();
     });
     checkbox.type = 'checkbox';
     checkbox.checked = task.completed;
@@ -48,4 +50,17 @@ function addListItem(task: Task) {
     label.append(checkbox, task.title);
     item.append(label);
     list?.append(item);
+}
+
+function saveTasks() {
+    localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem('TASKS');
+    if (taskJSON == null) {
+        return [];
+    }
+
+    return JSON.parse(taskJSON);
 }
