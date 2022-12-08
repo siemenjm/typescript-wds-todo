d0229a4f773bcd336f18ef4671aae8cfaf1a1f9d
Jared Siemen <siemenjm@gmail.com>
2022-12-07 19:46:31 -0500

    Grab list, form, and input in index.ts. Add submit event listener to form

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hi')
+// import { v4 as uuidV4 } from 'uuid';
+
+// console.log(uuidV4());
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.getElementById('#new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener("submit", e => {
+    e.preventDefault();
+
+    if (input?.value == '' || input?.value == null) {
+        return;
+    }
+
+
+});
