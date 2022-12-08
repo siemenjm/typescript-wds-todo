668eee4cfe9c997a2888e7a3ff0794aeebd5355f
Jared Siemen <siemenjm@gmail.com>
2022-12-07 20:09:09 -0500

    Add ability to add tasks that have completed/uncompleted checkbox

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { v4 as uuidV4 } from 'uuid';
 
+type Task = {
+    id: string,
+    title: string,
+    completed: boolean,
+    createdAt: Date
+}
+
 const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.getElementById('#new-task-form') as HTMLFormElement | null;
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
 form?.addEventListener("submit", e => {
@@ -11,10 +18,23 @@ form?.addEventListener("submit", e => {
         return;
     }
 
-    const task = {
+    const newTask: Task = {
         id: uuidV4(),
         title: input.value,
         completed: false,
         createdAt: new Date()
     };
+
+    addListItem(newTask);
 });
+
+function addListItem(task: Task) {
+    const item = document.createElement('li');
+    const label = document.createElement('label');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    label.append(checkbox, task.title);
+    item.append(label);
+    list?.append(item);
+}
